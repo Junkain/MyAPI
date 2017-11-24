c53828bd67902e3dffe4c1064719c03b6a1bccab
Erick Mori <erick.mori21@outlook.com>
2017-11-24 08:51:36 -0200

    Primeiro Commit

---
+++ MeuAPI/Controllers/PedidoController.cs
@@ -0,0 +1,52 @@
+﻿using MeuAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MeuAPI.Controllers
+{
+
+    public class PedidoController : ApiController
+    {
+
+        [HttpGet]
+        public List<Pedido> ObterPedidos()
+        {
+            Pedido p;
+
+            List<Pedido> pedidos = new List<Pedido>();
+
+            p = new Pedido();
+
+            p.id = 1;
+            p.descricao = "Coxinha";
+            p.valorTotal = 3.15;
+            pedidos.Add(p);
+
+            p = new Pedido();
+
+            p.id = 2;
+            p.descricao = "Hamburgão";
+            p.valorTotal = 5.00;
+            pedidos.Add(p);
+
+            p = new Pedido();
+
+            p.id = 3;
+            p.descricao = "Esiha";
+            p.valorTotal = 4.00;
+            pedidos.Add(p);
+
+            return pedidos;
+
+        }
+    }
+}
+
+
+
+
+
---
+++ MeuAPI/Models/Pedido.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MeuAPI.Models
+{
+    public class Pedido 
+    {
+        public int id { get; set; }
+        public string descricao { get; set; }
+        public double valorTotal { get; set; }
+
+    }
+}
